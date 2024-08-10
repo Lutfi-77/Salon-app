d33c55cfd49cce7aa2f9d3f362e974e8c38c227c
Lutfi-77 <xenonoffic@gmail.com>
2024-08-10 14:47:55 +0700

    update admin transaction

--- resources/views/admin/transaction/index.blade.php
+++ resources/views/admin/transaction/index.blade.php
@@ -27,6 +27,16 @@
 @endphp
 @extends('admin.template.app')
 
+@section('css')
+    <link rel="stylesheet" href="https://cdn.datatables.net/datetime/1.5.3/css/dataTables.dateTime.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/3.1.1/css/buttons.dataTables.css">
+    <style>
+        body.dt-print-view h1{
+            text-align: center;
+        }
+    </style>
+@endsection
+
 @section('title', 'Admin Appointment')
 
 @section('content')
@@ -40,26 +50,49 @@
 </div>
 <div class="mt-5 col-span-12 rounded-sm border border-stroke bg-white py-6 shadow-default dark:border-strokedark dark:bg-boxdark xl:col-span-4">
     <div class="container mx-auto">
+        {{-- {{dd($transactions[0]->appointment->detail)}} --}}
+        <table border="0" cellspacing="5" cellpadding="5">
+            <tbody>
+                <tr>
+                    <td>Minimum date:</td>
+                    <td><input class="appearance-none border-2 border-gray-200 rounded-xl w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-primary" type="text" id="min" name="min"></td>
+                </tr>
+                <tr>
+                    <td>Maximum date:</td>
+                    <td><input class="appearance-none border-2 border-gray-200 rounded-xl w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-primary" type="text" id="max" name="max"></td>
+                </tr>
+            </tbody>
+        </table>
         <table id="transactionTable" class="display" style="width:100%">
             <thead>
                 <tr>
                     <th>Customer</th>
+                    <th>Treatment</th>
                     <th>Total Price</th>
+                    <th>Date</th>
                     <th>Status</th>
-                    <th>Action</th>
+                    <th class="print:hidden">Action</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($transactions as $transaction)
                 <tr>
                     <td>{{$transaction->user->name}}</td>
+                    <td>
+                        @php
+                            $treatments = $transaction->appointment->detail->pluck('treatment');
+                            $treatmentList = $treatments->implode(', ');
+                        @endphp
+                        {{ $treatmentList }}
+                    </td>
                     <td>{{format_rupiah($transaction->total_price)}}</td>
+                    <td>{{$transaction->created_at->toDateString()}}</td>
                     <td>
                         <div class="font-bold px-2 py-1 rounded-lg text-center {{getStatusClass($transaction->status)}}">
                             {{$transaction->status}}
                         </div>
                     </td>
-                    <td>
+                    <td class="print:hidden">
                         <a href="{{ route('admin.transaction.invoice', $transaction->id) }}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg text-sm">
                             Cetak
                         </a>
@@ -70,9 +103,11 @@
             <tfoot>
                 <tr>
                     <th>Customer</th>
+                    <th>Treatment</th>
                     <th>Total Price</th>
+                    <th>Date</th>
                     <th>Status</th>
-                    <th>Action</th>
+                    <th class="print:hidden">Action</th>
                 </tr>
             </tfoot>
         </table>
@@ -81,8 +116,66 @@
 @endsection
 
 @push('js')
+    {{-- <script src="https://cdn.datatables.net/plug-ins/2.1.3/filtering/row-based/range_dates.js"></script> --}}
+    <script src="https://cdn.datatables.net/datetime/1.5.3/js/dataTables.dateTime.js"></script>
+    {{-- <script type="text/javascript" src="https://cdn.datatables.net/searchbuilder/1.0.1/js/dataTables.searchBuilder.min.js"></script> --}}
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.2/moment.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/datetime/1.5.3/js/dataTables.dateTime.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/3.1.1/js/buttons.dataTables.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/3.1.1/js/dataTables.buttons.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/3.1.1/js/buttons.print.min.js"></script>
+
+    <script type="text/javascript" src="https://cdn.datatables.net/buttons/3.1.1/js/buttons.html5.min.js"></script>
     <script>
-        let table = new DataTable('#transactionTable');
+        let minDate, maxDate;
+ 
+        // Custom filtering function which will search data in column four between two values
+        DataTable.ext.search.push(function (settings, data, dataIndex) {
+            let min = minDate.val();
+            let max = maxDate.val();
+            let date = new Date(data[2]);
+        
+            if (
+                (min === null && max === null) ||
+                (min === null && date <= max) ||
+                (min <= date && max === null) ||
+                (min <= date && date <= max)
+            ) {
+                return true;
+            }
+            return false;
+        });
+        
+        // Create date inputs
+        minDate = new DateTime('#min', {
+            format: 'MMMM Do YYYY'
+        });
+        maxDate = new DateTime('#max', {
+            format: 'MMMM Do YYYY'
+        });
+        
+        // DataTables initialisation
+        let table = new DataTable('#transactionTable',{
+            responsive: true,
+            layout: {
+                topStart: {
+                    buttons: [
+                        {
+                            extend: 'print',
+                            className: 'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg text-sm',
+                            exportOptions: {
+                                columns: [0, 1, 2, 3]
+                            }
+                        },
+                    ]
+                }
+            }
+        });
+        
+        // Refilter the table
+        document.querySelectorAll('#min, #max').forEach((el) => {
+            el.addEventListener('change', () => table.draw());
+        });
     </script>
     {{-- <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
     <script>
