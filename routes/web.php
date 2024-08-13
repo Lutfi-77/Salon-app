68e000a0378b101c2593a37f8d5372c0fa3262b6
Lutfi-77 <xenonoffic@gmail.com>
2024-08-13 07:27:17 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -32,10 +32,10 @@ Route::get('/register', [AuthUserController::class, 'register'])->name('user.reg
 Route::post('/register', [AuthUserController::class, 'storeRegister'])->name('user.registerStore');
 
 // USER ROUTES
+Route::resource('profile', UserProfileController::class);
 Route::middleware(['role:customer', 'profileCheck'])->prefix('user')->group(function () {
     Route::get('dashboard', [UserDashboardController::class, 'index'])->name('user.dashboard');
     Route::post('logout', [AuthUserController::class, 'logout'])->name('user.logout');
-    Route::resource('profile', UserProfileController::class);
     // Route::post('logout', [AuthUserController::class, 'logout'])->name('user.logout');
 
     Route::get('appointment', [AppointmentController::class, 'index'])->name('user.appointment');
